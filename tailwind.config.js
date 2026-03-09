4b0cb39fe514072a45dd01757475ea694be24598
giung1 <agus_98_jov@hotmail.com.ar>
2026-03-09 18:19:50 -0300

    initial commit: landing done and contact page too, just the netflify forms config left

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        oranienbaum: ["Oranienbaum", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
